fa53046c344b2c804c50fcc2262cf1f3ba071b63
Hazerin <alby91v@gmail.com>
2025-08-05 21:12:29 +0200

    checkpoint

--- FAQ-accordion/src/components/layouts/Layout.vue
+++ FAQ-accordion/src/components/layouts/Layout.vue
@@ -3,20 +3,22 @@
 </script>
 
 <template>
-    <header>
-
-    </header>
-    <main>
-        <slot/>
-    </main>
-    <footer>
-        <div class="attribution">
-            Challenge by <a href="https://www.frontendmentor.io?ref=challenge" target="_blank">Frontend Mentor</a>. 
-            Coded by <a href="#">Your Name Here</a>.
-        </div>
-    </footer>
+    <body class="min-h-screen flex flex-col justify-between items-center">
+        <header>
+    
+        </header>
+        <main>
+            <slot/>
+        </main>
+        <footer>
+            <div class="attribution">
+                Challenge by <a href="https://www.frontendmentor.io?ref=challenge" target="_blank">Frontend Mentor</a>. 
+                Coded by <a href="#">Your Name Here</a>.
+            </div>
+        </footer>
+    </body>
 </template>
 
 <style scoped>
-
+    
 </style>
