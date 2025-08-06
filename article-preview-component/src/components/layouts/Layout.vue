8f59f943734022ca5d322f57882c6d95176f283b
Hazerin <alby91v@gmail.com>
2025-08-06 19:47:54 +0200

    completate le basi

---
+++ article-preview-component/src/components/layouts/Layout.vue
@@ -0,0 +1,30 @@
+<script setup>
+
+</script>
+
+<template>
+    <body class="min-h-screen">
+        <header>
+            
+        </header>
+        <main>
+            <slot/>
+        </main>
+        <footer>
+            <div class="attribution">
+                Challenge by <a href="https://www.frontendmentor.io?ref=challenge" target="_blank">Frontend Mentor</a>. 
+                Coded by <a href="#">Hazerin</a>.
+            </div>
+        </footer>
+    </body>
+</template>
+
+<style scoped>
+    .attribution { 
+        font-size: 11px; 
+        text-align: center; 
+    }
+    .attribution a { 
+        color: hsl(228, 45%, 44%); 
+    }
+</style>
