d49519f2abf9d4d3f2edf5a0a4b81f1e77a5df50
Hazerin <alby91v@gmail.com>
2025-08-24 16:03:30 +0200

    checkpoint

---
+++ huddle-landing-page/src/components/layouts/Layout.vue
@@ -0,0 +1,26 @@
+<script setup>
+    
+</script>
+
+<template>
+  <header class="h-[373px]">
+      <img src="/bg-mobile.svg" alt="bg-mobile" class="absolute z-[-10]">
+      <div class="flex flex-col px-7 items-start pt-6 gap-12">
+        <img src="/logo.svg" alt="bg-mobile" class="h-10">
+        <img src="/illustration-mockups.svg" alt="bg-mobile" class="z-10">
+      </div>
+  </header>
+  <body class="flex flex-col flex-1">
+    <slot/>
+  </body>
+  <footer>
+    <p class="attribution">
+      Challenge by <a href="https://www.frontendmentor.io?ref=challenge" target="_blank">Frontend Mentor</a>. 
+      Coded by <a href="#">Hazerin</a>.
+    </p>
+  </footer>
+</template>
+
+<style scoped>
+
+</style>
---
+++ huddle-landing-page/src/components/pages/Main.vue
@@ -0,0 +1,17 @@
+<script setup>
+
+</script>
+
+<template>
+  <section class="text-white px-7 flex flex-col flex-1 items-center justify-center min-h-full">
+    <h1 class="font-semibold">Build The Community Your Fans Will Love</h1>
+    <p class="">Huddle re-imagines the way we build communities. You have a voice, but so does
+        your audience. Create connections wit your users as you engage in genuine discussions.
+    </p>
+    <button class="bg-white text-purple-700 rounded-full py-2 px-20">Register</button>
+  </section>
+</template>
+
+<style scoped>
+
+</style>
