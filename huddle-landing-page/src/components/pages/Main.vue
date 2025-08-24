a0505496fb18cc1e0d80eadafd7e635e340f6e85
Hazerin <alby91v@gmail.com>
2025-08-24 16:12:49 +0200

    Update Main.vue

--- huddle-landing-page/src/components/pages/Main.vue
+++ huddle-landing-page/src/components/pages/Main.vue
@@ -3,10 +3,10 @@
 </script>
 
 <template>
-  <section class="text-white px-7 flex flex-col flex-1 items-center justify-center min-h-full">
-    <h1 class="font-semibold">Build The Community Your Fans Will Love</h1>
+  <section class="text-white px-11 flex flex-col flex-1 items-center justify-center gap-5 text-center">
+    <h1 class="font-bold text-2xl">Build The Community Your Fans Will Love</h1>
     <p class="">Huddle re-imagines the way we build communities. You have a voice, but so does
-        your audience. Create connections wit your users as you engage in genuine discussions.
+        your audience. Create connections with your users as you engage in genuine discussions.
     </p>
     <button class="bg-white text-purple-700 rounded-full py-2 px-20">Register</button>
   </section>
