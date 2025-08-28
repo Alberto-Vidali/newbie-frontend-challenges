38ae02e93d7f597b551976b7e1ae81f4915bab00
Hazerin <alby91v@gmail.com>
2025-08-28 22:20:00 +0200

    new challenge

---
+++ result-summary-component/src/components/layouts/Layout.vue
@@ -0,0 +1,23 @@
+<script setup>
+    
+</script>
+
+<template>
+    <header class="hidden">
+
+    </header>
+    <main class="flex flex-col justify-center flex-1">
+        <slot/>
+    </main>
+    <footer>
+        <div class="attribution">
+            Challenge by <a href="https://www.frontendmentor.io?ref=challenge" target="_blank">Frontend Mentor</a>. 
+            Coded by <a href="#">Alberto Vidali</a>.
+        </div>
+    </footer>
+</template>
+
+<style scoped>
+    .attribution { font-size: 13px; text-align: center; }
+    .attribution a { color: hsl(228, 45%, 44%); }
+</style>
