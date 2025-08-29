e97eeb2ea4ab9194640f10f3ddd86bf341c4b707
Hazerin <alby91v@gmail.com>
2025-08-29 17:48:02 +0200

    Update Main.vue

--- result-summary-component/src/components/pages/Main.vue
+++ result-summary-component/src/components/pages/Main.vue
@@ -3,29 +3,33 @@
 </script>
 
 <template>
-    <section>
-        Your Result
-        76
-        of 100
+    <section class="flex flex-col flex-1">
+        <div class=" flex flex-col items-center justify-between flex-3 bg-gradient-to-b from-purple-700 to-blue-700 rounded-b-3xl text-white">
+            <h2 class="">Your Result</h2>
+            <div>
+                <h1>76</h1>
+                <p>of 100</p>
+            </div>
+            <h1>Great</h1>
+            <p>You scored higher than 65% of the people who have taken these tests.</p>
+        </div>
+        <div class="flex-7">
+            Summary
 
-        Great
-        You scored higher than 65% of the people who have taken these tests.
-        
-        Summary
+            Reaction
+            80 / 100
 
-        Reaction
-        80 / 100
+            Memory
+            92 / 100
 
-        Memory
-        92 / 100
+            Verbal
+            61 / 100
 
-        Verbal
-        61 / 100
+            Visual
+            72 / 100
 
-        Visual
-        72 / 100
-
-        Continue
+            Continue
+        </div>
     </section>
 </template>
 
